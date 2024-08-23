ca2fe4eb595d25ec742e15c6813acecbc9d3730f
Erasmo-Vibras <erasmo.vibras@gmail.com>
2024-08-22 21:46:50 -0300

    feat: add insta and face icons and links

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -6,30 +6,44 @@ import { theme } from '@styles/theme'
 import { LogoImage } from './styles'
 import { Paragraph } from '@components/paragraph'
 import { Footer } from '@components/footer'
+import { InstagramIcon } from '@assets/icons/InstagramIcon'
+import { FacebookIcon } from '@assets/icons/FacebookIcon'
+import LogoBrancaFundoTransparente from '@assets/images/LogoBrancaFundoTransparente.png'
 
 export default function Home() {
   return (
-    <Flex 
+    <Flex
       flex={1}
       direction='column'
       bgColor={theme.colors.primary}
+      align='center'
     >
-      <Flex height ='50%' justify='center'>
-        <LogoImage/>
+      <Flex height='50%' justify='center'>
+        <LogoImage src={LogoBrancaFundoTransparente}/>
       </Flex>
-      <Flex direction='column' align='center' height='50%'>
+      <Flex direction='column' align='center'>
         <Paragraph color={theme.colors.white} size={theme.fontSize.h5}>
-          Tecnologia voltada a equidade social
+          Tecnologia voltada à equidade social
         </Paragraph>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <Paragraph color={theme.colors.white}  size={theme.fontSize.h5}>
-          Acompanhe a Vibras Tecnologias nas redes sociais para mais novidades
+        <br />
+        <br />
+        <br />
+        <br />
+        <Paragraph color={theme.colors.white} size={theme.fontSize.h5}>
+          Acompanhe nossas redes sociais para mais novidades
         </Paragraph>
-      <Footer/>
       </Flex>
+      <br />
+      <br />
+      <Flex width='30%' justify='space-evenly'>
+        <a href="https://instagram.com/vibrastecnologia">
+          <InstagramIcon />
+        </a>
+        <a href="https://www.facebook.com/100095027293080">
+          <FacebookIcon />
+        </a>
+      </Flex>
+      <Footer />
     </Flex>
   )
 }
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -3,12 +3,7 @@ import styled from 'styled-components'
 import { Flex } from '@components/flex'
 import LogoBrancaFundoTransparente from '@assets/images/LogoBrancaFundoTransparente.png'
 
-export const LogoImage = styled(Flex)`
-  width: 100%;
-  height: 100%;
-  
-  background-repeat: no-repeat;
-  background-image: url(${LogoBrancaFundoTransparente});
+export const LogoImage = styled.img`
 
   // @media (max-width: 1024px) {
   //   display: none;
