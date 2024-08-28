045b5610ee5dca76bf63565f43dffad63b0b05c5
Erasmo-Vibras <erasmo.vibras@gmail.com>
2024-08-28 12:44:26 -0300

    fix: base path

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,8 +5,8 @@ import Home from '@screens/home'
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route index path='/' element={<Home />} />
-      <Route path='*' element={<Navigate to={'/'} />} />
+      <Route index path='/LandingPage' element={<Home />} />
+      <Route path='*' element={<Navigate to={'/LandingPage'} />} />
     </Routes>
   )
 }
